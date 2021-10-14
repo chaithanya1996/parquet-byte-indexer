d01e21a8b90a17412709523db970df06f6050200
Adapa, Venkata Sai Chaitanya (Chaitanya) <venkatasaichaitanya.adapa@ge.com>
2021-10-14 19:25:10 +0530

    Starting Commit for Parquet file conversion Binary Program

---
+++ configure_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo " Compiling Arrow: "
+mkdir -p build/arrow
+mkdir libs
+cmake -S arrow/cpp/  -B build/arrow \
+                -DARROW_BUILD_SHARED=OFF \
+                -DARROW_BUILD_STATIC=ON \
+                -DARROW_CSV=ON \
+                -DARROW_DEPENDENCY_SOURCE=BUNDLED \
+                -DARROW_DEPENDENCY_USE_SHARED=OFF \
+                -DARROW_PARQUET=ON \
+                -DARROW_WITH_BROTLI=ON \
+                -DARROW_WITH_BZ2=ON \
+                -DARROW_WITH_LZ4=ON \
+                -DARROW_WITH_SNAPPY=ON \
+                -DARROW_WITH_ZLIB=ON \
+                -DARROW_WITH_ZSTD=ON
+
+make -j2 -C build/arrow
+
+make DESTDIR="../../include_libs" install -j4 -C build/arrow
+make  uninstall -j4 -C build/arrow
